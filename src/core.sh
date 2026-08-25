268c7206abff3f7a3204274b0f3fdd4b940fb5c2
noa1188 <53296477+noa1188@users.noreply.github.com>
2026-08-25 15:01:37 +0000

    Update core.sh

--- src/core.sh
+++ src/core.sh
@@ -160,15 +160,15 @@ get_pbk() {
 }
 
 generate_short_id() {
-    id=$($is_core_bin generate rand 8 --hex 2>/dev/null)
-    [[ ! $id ]] && id=$(openssl rand -hex 8 2>/dev/null)
-    [[ ! $id ]] && id=$(od -An -N8 -tx1 /dev/urandom 2>/dev/null | tr -d ' \n')
-    [[ ! $id ]] && id=$(date +%N | cut -c1-16)
-    [[ ! $id || ${#id} -lt 16 ]] && id=$(head -c 8 /dev/urandom | od -An -tx1 | tr -d ' \n')
-    [[ ! $id || ${#id} -lt 16 ]] && id=$(dd if=/dev/urandom bs=1 count=8 2>/dev/null | xxd -p 2>/dev/null)
-    [[ ! $id || ${#id} -lt 16 ]] && id=$(cat /dev/urandom | tr -dc 'a-f0-9' | head -c 16)
-    [[ ! $id || ${#id} -lt 16 ]] && id=$(printf '%016x' $((RANDOM * RANDOM * RANDOM)))
-    [[ ! $id || ${#id} -lt 16 ]] && id=$(uuidgen 2>/dev/null | tr '-' '' | head -c 16)
+    read -r id < <($is_core_bin generate rand 8 --hex 2>/dev/null)
+    [[ ! $id ]] && read -r id < <(openssl rand -hex 16 2>/dev/null)
+    [[ ! $id || ${#id} -lt 16 ]] && read -r id < <(od -An -tx1 /dev/urandom | tr -d ' \n' | head -c 16)
+    [[ ! $id || ${#id} -lt 16 ]] && read -r id < <(date +%s%N | cut -c3-18)
+    [[ ! $id || ${#id} -lt 16 ]] && read -r id < <(head -c 8 /dev/urandom | od -An -tx1 | tr -d ' \n')
+    [[ ! $id || ${#id} -lt 16 ]] && read -r id < <(dd if=/dev/urandom bs=1 count=8 2>/dev/null | xxd -p)
+    [[ ! $id || ${#id} -lt 16 ]] && read -r id < <(cat /dev/urandom | tr -dc 'a-f0-9' | head -c 16)
+    [[ ! $id || ${#id} -lt 16 ]] && read -r id < <(printf '%016x' $((RANDOM * RANDOM * RANDOM)))
+    [[ ! $id || ${#id} -lt 16 ]] && read -r id < <(uuidgen 2>/dev/null | tr '-' '' | head -c 16)
     printf '%s\n' "$id"
 }
 
