a4a4ceb0a4c81535526fae2d36a53693fd93e08b
noa1188 <53296477+noa1188@users.noreply.github.com>
2026-08-26 08:04:02 +0000

    Update core.sh

--- src/core.sh
+++ src/core.sh
@@ -1254,6 +1254,7 @@ get() {
             	is_short_id=$(printf '%016x' $RANDOM)
             fi
             is_json_add="tls:{enabled:true,server_name:\"$is_servername\",reality:{enabled:true,handshake:{server:\"$is_servername\",server_port:443},private_key:\"$is_private_key\",short_id:[\"$is_short_id\"]}}"
+            [[ -z $is_short_id ]] && is_short_id=$(printf '%016x' $RANDOM)
             [[ $is_lower =~ "http" ]] && {
                 is_json_add="$is_json_add,transport:{type:\"http\"}"
             } || {
